c8521e16c7a73ca3945eccf0633cad70c0068664
Pooja Bijwal <bijwalpooja@gmail.com>
2022-10-01 04:10:28 +0530

    Add

--- Home page/Script/index.js
+++ Home page/Script/index.js
@@ -67,81 +67,105 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+let data_slider_2 = ["https://www.netmeds.com/images/cms/aw_rbslider/slides/1664560049_Dabur_Home.jpg","https://www.netmeds.com/images/cms/aw_rbslider/slides/1664559741_Emami_Mini-banner-web.jpg","https://www.netmeds.com/images/cms/aw_rbslider/slides/1664111731_Sri_Sri_Tattva_Mini_banner_web.jpg","https://www.netmeds.com/images/cms/aw_rbslider/slides/1664560087_Himalaya_Mini-banner-web.jpg","https://www.netmeds.com/images/cms/aw_rbslider/slides/1664560311_Kerala-Ayurveda_-wellness--main-banner-mobile-.jpg","https://www.netmeds.com/images/cms/aw_rbslider/slides/1664560311_Kerala-Ayurveda_-wellness--main-banner-mobile-.jpg","https://www.netmeds.com/images/cms/aw_rbslider/slides/1664560461_Baidyanath_Mini_banner_web.jpg","https://www.netmeds.com/images/cms/aw_rbslider/slides/1664560825_Kapiva_Mobile_home.jpg","https://www.netmeds.com/images/cms/aw_rbslider/slides/1661967766_Accusure_Mini_banner_web.jpg"]
 
-let data_arr_2 = ["https://www.netmeds.com/images/cms/aw_rbslider/slides/1663139989_436x224_copy.jpg","https://www.netmeds.com/images/cms/aw_rbslider/slides/1660801057_Wellness_Mini.jpg","https://www.netmeds.com/images/cms/aw_rbslider/slides/1660801237_Ayurverda_Mini.jpg","https://www.netmeds.com/images/cms/aw_rbslider/slides/1661963801_Web_Mini_Bannerffewr.jpg"]
+let arr1=[];
+let arr2=[];
+let arr3=[];
 
-// function SlideShow_2(){
- 
-//     let slidshow = document.querySelector("#Slider_2");
+let start;
+let end;
+   
+function slider(){
+  
+    start=0;
+    end=(data_slider_2.length/3);
+    arr1=[]
+    for(let i=start;i<end;i++){
+        arr1.push(data_slider_2[i]);
+    }
+    for(let i=3;i<(data_slider_2.length/3)+1;i++){
+        arr2.push(data_slider_2[i]);
+    }
+    for(let i=7;i<(data_slider_2.length/3)+1;i++){
+        arr3.push(data_slider_2[i]);
+    }
+    // for(let i=15;i<(data.length/4)+15;i++){
+    //     arr4.push(data[i]);
+    // }
+    console.log(arr1,arr2,arr3)
+    append(arr1,arr2,arr3)
+
+    setInterval(function goNxt(){
+
+    },3000)
+}
+    
+slider()
 
 
-//      slidshow.innerHTML = null;
 
+
+
+
+function goNxt(){
+  setInterval(function(){
+    let arr1=[]
+    start+=1;
+    end+=1;
+    if(end > data_slider_2.length){
+        end=(data_slider_2.length/3);
+    }
+    if(start > data_slider_2.length - 3){
+        start=0;
+    }
+    for(let i = start; i < end; i++){
+        arr1.push(data_slider_2[i]);
+    }
+    append(arr1)
+
+  },3000)
+}
     
 
-//     let div1 = document.createElement("div")
-//     let img1 = document.createElement("img");
 
-//     let div2 = document.createElement("div")
-//     let img2 = document.createElement("img");
-
-//     let div3 = document.createElement("div")
-//     let img3 = document.createElement("img");
-
-//     let div4 = document.createElement("div")
-//     let img4 = document.createElement("img");
-     
+function goback(){
+  setInterval(function(){
+    let arr1=[]
     
-//      for(let i=0;i<3;i++)
-//      {
-//         div1.append(img1)
-//         img1.src = data_arr_2[i]; 
-//     }
+    end-=1;
+    start=end-2;
+    if(end<3){
+        end=data_slider_2.length-1;
+        start=(data_slider_2.length-1)-2;
+    }
+    // if(start<5){
+    //     start=data.length-5;
+    // }
+    for(let i=end;i>=start;i--){
+        arr1.push(data_slider_2[i]);
+    }
+    append(arr1)
 
 
-// //     let i = 0;
-      
-// //      i++;
+  },3000)
+}
     
-// //         div1.append(img1)
-// //         img1.src = data_arr_2[0];
+// document.getElementById("left_btn").addEventListener("click",goback);
+// document.getElementById("right_btn").addEventListener("click",goNxt);
 
-// //         div2.append(img2)
-// //         img2.src = data_arr_2[1];
+function append(data_slider_2){
+    let container=document.getElementById("slider_3");
+    container.innerHTML=null;
 
-// //         div3.append(img3)
-// //         img3.src = data_arr_2[2];
+    data_slider_2.forEach(function(el){
+        let div=document.createElement("div");
+        div.setAttribute("class","pBox");
+        let img=document.createElement("img");
+        img.src=el;
+        
+        div.append(img);
+        container.append(div)
+    });
 
-// //         slidshow.append(div1,div2,div3)
-
-
-// //    let id = setInterval(function(){
-
-// //     if(i==3)
-// //     {
-// //          i=0;
-// //     }
-     
-    
-       
-// //     div2.append(img2)
-// //     img2.src = data_arr_2[1];
-
-// //     div3.append(img3)
-// //     img3.src = data_arr_2[2];
-
-// //     div4.append(img4)
-// //     img4.src = data_arr_2[3];
-
-// //     slidshow.append(div2,div3,div4)
-
-// //     console.log(i)
-    
-     
-// //     i++;
-
-    
-// //     },1000)
-// // }
-
-// SlideShow_2(data_arr_2)
+}
