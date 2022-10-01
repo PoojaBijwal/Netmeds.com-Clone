79a11a1a876dd944c84be703f809c86ce9f75147
rajkumarsahu89 <sahukumarraj89@gmail.com>
2022-10-02 00:48:47 +0530

    raj

---
+++ scripts/login.js
@@ -0,0 +1,26 @@
+import footer from "./footer.js"
+   import navbar from "./navbar.js"
+  // import { footer } from "./JS/footer.js";
+  let container = document.getElementById("footer");
+  container.innerHTML = footer();
+  let cont = document.getElementById("navbar")
+  cont.innerHTML = navbar()
+  console.log(navbar())
+  // console.log(container);
+
+
+let inp = document.querySelector("#number");
+console.log(inp);
+
+let btn = document.querySelector(".otp_btn");
+console.log(btn);
+let a = Math.round(Math.random() * 10000 + 123456);
+btn.addEventListener("click", () => {
+  if (inp.value) {
+    localStorage.setItem("otp", a);
+    alert("Your OTP is " + a + ".");
+    window.location.assign("index.html");
+  }
+});
+console.log(footer())
+console.log(navbar())
